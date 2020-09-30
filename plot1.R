6c7e5626c8bcb7362d2cf7ff06e60df1b86b8e17
hrithik-infi <72163087+hrithik-infi@users.noreply.github.com>
2020-10-01 00:33:56 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -2,6 +2,6 @@
 pow_data <- read.table('power_data.txt', header=TRUE, sep=";", stringsAsFactors=FALSE)
 date_set <- pow_data[pow_data$Date %in% c("1/2/2007","2/2/2007"),]
 G_A_P <- as.numeric(date_set$Global_active_power)
-png("plot1.png", width=480, height=480)
+png("plot1.png", width=540, height=540)
 hist(G_A_P, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
 dev.off()
